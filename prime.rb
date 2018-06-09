8e20b9025b57e1da55c92d57a00950d366ca6e6e
Miriam Castillo <castillo.m.mc@gmail.com>
2018-06-09 22:33:13 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -13,14 +13,27 @@
 #     return true
 #  end
 # end
-def prime?(n)
-  if n < 2
-        false
-    elsif n == 3
-        true
-    elsif n.modulo(2) == 0 || n.modulo(3) == 0
-        false
-    else
-        true
+# def prime?(n)
+#   if n < 2
+#         false
+#     elsif n == 3
+#         true
+#     elsif n.modulo(2) == 0 || n.modulo(3) == 0
+#         false
+#     else
+#         true
+#   end
+# end
+
+def prime?(num)
+  if num < 0 or num == 0 or num == 1
+    return false
+  else
+    (2..num-1).to_a.all? do |possible_factor|
+      num % possible_factor != 0
+    end
   end
 end
+
+puts prime?(105557)
+
