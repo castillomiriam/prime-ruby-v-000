458e9a49c97468850296b4ac0d3eb05b09e74f97
Miriam Castillo <castillo.m.mc@gmail.com>
2018-06-09 22:19:44 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -3,6 +3,9 @@ def prime?(number)
   if number <= 1
     return false
   elsif
+    number == 3
+    return true
+  elsif
     # [2,3,4,5,6].each do |num|
       number.modulo(2) == 0 || number.modulo(3) == 0
       return false
