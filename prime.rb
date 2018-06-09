2ad9b65d74106db8866f515886b4ab37df827b6a
Miriam Castillo <castillo.m.mc@gmail.com>
2018-06-09 22:22:36 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,15 +1,26 @@
 # Add  code here!
-def prime?(number)
-  if number < 2
-    return false
-  elsif
-    number == 3
-    return true
-  elsif
-    # [2,3,4,5,6].each do |num|
-      number.modulo(2) == 0 || number.modulo(3) == 0
-      return false
-  else
-    return true
- end
+# def prime?(number)
+#   if number < 2
+#     return false
+#   elsif
+#     number == 3
+#     return true
+#   elsif
+#     # [2,3,4,5,6].each do |num|
+#       number.modulo(2) == 0 || number.modulo(3) == 0
+#       return false
+#   else
+#     return true
+#  end
+# end
+def prime?(n)
+  if n < 2
+        false 
+    elsif n == 3
+        true
+    elsif n.modulo(2) == 0 || n.modulo(3) == 0
+        false
+    else
+        true  
+  end
 end
